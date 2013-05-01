e40bc370a9bb0bd43b80044e3195f90e96afdb25
YICHI ZHANG <zhych125@gmail.com>
2013-04-30 18:04:24 -0700

    Initial Commit

---
+++ HuffmanCompressing/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HuffmanCompressing
+//
+//  Created by YICHI ZHANG on 4/30/13.
+//  Copyright (c) 2013 YICHI ZHANG. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
