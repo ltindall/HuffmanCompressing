4ed0d383cb691fb8d4ab840af8723c41b7ad43ad
YICHI ZHANG <zhych125@gmail.com>
2013-05-01 18:46:41 -0700

    InputOutput Implemented

--- HuffmanCompressing/BitInputStream.cpp
+++ HuffmanCompressing/BitInputStream.cpp
@@ -6,4 +6,41 @@
 //  Copyright (c) 2013 YICHI ZHANG. All rights reserved.
 //
 
-#include "BitInputStream.h"
+#include "BitInputStream.hpp"
+
+int BitInputStream::readBit()
+{
+    if (bufi==8)
+    {
+        if(in.eof())
+            return -1;
+        buf=in.get();
+        bufi=0;
+    }
+    bufi++;
+    int i = buf>>7;
+    buf<<=1;
+    return i;
+    
+}
+
+int BitInputStream::readByte()
+{
+    if(in.eof())
+        return -1;
+    return in.get();
+}
+
+int BitInputStream::readInt()
+{
+    int i,temp;
+    for (int j=0;j<4;j++)
+    {
+        if (in.eof())
+            return -1;
+        temp=in.get();
+        temp<<=((3-j)*8);
+        i|=temp;
+    }
+    return i;
+}
--- HuffmanCompressing/BitOutputStream.cpp
+++ HuffmanCompressing/BitOutputStream.cpp
@@ -6,4 +6,39 @@
 //  Copyright (c) 2013 YICHI ZHANG. All rights reserved.
 //
 
-#include "BitOutputStream.h"
+#include "BitOutputStream.hpp"
+
+void BitOutputStream::writeBit(int bit)
+{
+    
+    if (bufi==8)
+        flush();
+    buf|=bit;
+    buf<<=1;
+    bufi++;
+}
+
+void BitOutputStream::writeByte(int b)
+{
+    out.put((char)b);
+}
+
+void BitOutputStream::writeInt(int i)
+{
+    for (int j=0;j<4;j++)
+    {
+        char tempbuf=(char)(i>>((3-j)*8));
+        out.put(tempbuf);
+    }
+}
+
+void BitOutputStream::flush()
+{
+    if (buf)
+    {
+        out.put(buf);
+        out.flush();
+        buf=bufi=0;
+    }
+}
+
