3fbbc81a28bcbc4db94df33e3d7bbb6488c71321
YICHI ZHANG <zhych125@gmail.com>
2013-05-01 18:45:06 -0700

    BitInput,Output Implemented

---
+++ HuffmanCompressing/BitInputStream.cpp
@@ -0,0 +1,9 @@
+//
+//  BitInputStream.cpp
+//  HuffmanCompressing
+//
+//  Created by YICHI ZHANG on 5/1/13.
+//  Copyright (c) 2013 YICHI ZHANG. All rights reserved.
+//
+
+#include "BitInputStream.h"
---
+++ HuffmanCompressing/BitOutputStream.cpp
@@ -0,0 +1,9 @@
+//
+//  BitOutputStream.cpp
+//  HuffmanCompressing
+//
+//  Created by YICHI ZHANG on 5/1/13.
+//  Copyright (c) 2013 YICHI ZHANG. All rights reserved.
+//
+
+#include "BitOutputStream.h"
